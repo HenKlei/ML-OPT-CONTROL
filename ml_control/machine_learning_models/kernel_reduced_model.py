78c252aa1143109d2907f69fca21645575a6214a
Hendrik Kleikamp <hendrik.kleikamp@uni-muenster.de>
2023-11-10 12:10:57 +0100

    fixed bug in shape of coefficients

--- ml_control/machine_learning_models/kernel_reduced_model.py
+++ ml_control/machine_learning_models/kernel_reduced_model.py
@@ -12,7 +12,7 @@ class KernelReducedModel(BasicReducedMachineLearningModel):
                          parametrized_xT, R_chol, M, 'KernelReducedModel', spatial_norm=spatial_norm,
                          zero_padding=zero_padding)
 
-    def train(self, kernel=Gaussian(0.1), greedy_type='p_greedy', tol_p=1e-10, max_iter=None):
+    def train(self, kernel=Gaussian(1.), greedy_type='p_greedy', tol_p=1e-10, max_iter=None):
         """Trains the VKOGA surrogate."""
         if self.zero_padding:
             training_data_x = []
@@ -42,8 +42,14 @@ class KernelReducedModel(BasicReducedMachineLearningModel):
                 t_y = np.array(t_y)
                 if t_x.ndim == 1:
                     t_x = t_x[..., np.newaxis]
-                k = VKOGA(kernel=kernel, greedy_type=greedy_type, tol_p=tol_p)
-                k = k.fit(t_x, t_y, maxIter=max_iter or len(t_x))
+                if len(t_x) > 0:
+                    k = VKOGA(kernel=kernel, greedy_type=greedy_type, tol_p=tol_p)
+                    k = k.fit(t_x, t_y, maxIter=max_iter or len(t_x))
+                else:
+                    class NullFunction:
+                        def predict(self, _):
+                            return np.zeros(1)
+                    k = NullFunction()
                 self.kernel_model.append(k)
 
     def get_coefficients(self, mu):
@@ -53,6 +59,6 @@ class KernelReducedModel(BasicReducedMachineLearningModel):
             if self.zero_padding:
                 return self.kernel_model.predict(converted_input).flatten()
             else:
-                return np.array([k.predict(converted_input).flatten() for k in self.kernel_model])
+                return np.array([k.predict(converted_input).flatten() for k in self.kernel_model]).flatten()
         else:
             return np.zeros(len(self.reduced_model.reduced_basis))
